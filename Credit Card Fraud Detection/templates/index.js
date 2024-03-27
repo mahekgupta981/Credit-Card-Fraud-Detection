7b482c3e053da898414b32dc22eb89a1e695512c
Mahek Gupta <142731565+mahekgupta981@users.noreply.github.com>
2024-03-27 13:12:10 +0530

    Add files via upload

---
+++ Credit Card Fraud Detection/templates/index.js
@@ -0,0 +1,7 @@
+function show(){var card = document.getElementById("card-number").value;
+var name = document.getElementById("name-text");
+var exp = document.getElementById("expiration");
+var cvv = document.getElementById("cvv");
+var submit = document.getElementById("submit");
+
+document.getElementById("Card_Number").innerHTML = card;}
